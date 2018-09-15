bd18b345a265825e15e299cba2c38b2fc55b7f21
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-09-15 14:45:54 +0530

    Create Variables.java

---
+++ Variables/Variables.java
@@ -0,0 +1,31 @@
+package Variables;
+
+public class Variables {
+ 
+     	
+	public static void main(String[] args) {
+		
+		
+		// variable is a name given to a memory location
+		  
+		String emp_name ="Guhan Ganesan";
+    System.out.println(emp_name);
+		/* Datatype = String 
+		 * variable name = emp_name
+		 * value = "Guhan Ganesan"
+		 */
+		
+		// Variable Types
+		/* 1. Local variable
+		 * 2. Instance variable
+		 * 3. Static Variable ( Refer Local.java, Instance.java, 
+		 *    Static.java
+		 * 
+		 */
+	}
+
+}
+
+
+Output:-
+        Guhan Ganesan
