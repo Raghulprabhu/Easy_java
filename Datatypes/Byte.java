9201872f48cc59899ded3e9b5e8b4298cf8ea7a6
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-09-16 13:19:24 +0530

    Update Byte.java

--- Datatypes/Byte.java
+++ Datatypes/Byte.java
@@ -5,11 +5,12 @@ public class Byte {
 	// 8 bit size and range is  -128 to 127
 	// short 16 bit
 	// int 32 bit
-    // long 64 bit
-	
+        // long 64 bit
 	// float ( 6 decimail places)
 	// double ( 16 decimal places)
 	// long double ( 19 deciaml places )
+	//Single precision (float) gives you 23 bits of significand, 8 bits of exponent, and 1 sign bit.
+	//Double precision (double) gives you 52 bits of significand, 11 bits of exponent, and 1 sign bit.
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
