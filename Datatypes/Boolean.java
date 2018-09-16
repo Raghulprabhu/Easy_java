3367ca2fc775f9671c284ba90da856a895e95a20
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-09-16 13:12:08 +0530

    Create Boolean.java

---
+++ Datatypes/Boolean.java
@@ -0,0 +1,23 @@
+package Datatypes;
+
+public class Boolean {
+      /*  boolean data type represents only one bit of information either true or fals
+       *  Cannot convert to other data types
+       * 
+       * 
+       */
+	
+	public static void main(String[] args) {
+		
+		boolean a=true;
+		boolean b=false;
+		
+		if(a==true)
+		{
+			System.out.println("Guhan");
+		}
+		
+
+	}
+
+}
