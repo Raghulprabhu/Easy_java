b60d87677140b13e0772298a9df6d7317139bfd5
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-09-17 15:24:56 +0530

    Update TestEnum1.java

--- Datatypes/TestEnum1.java
+++ Datatypes/TestEnum1.java
@@ -1,5 +1,10 @@
 package Datatypes;
 
+/*
+A Java Enum is a special Java type used to define collections of constants. More precisely,
+a Java enum type is a special kind of Java class. An enum can contain constants, methods etc.
+*/
+
 public class TestEnum1 {
 
 	public static void main(String[] args) {
