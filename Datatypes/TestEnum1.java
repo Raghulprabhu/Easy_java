7a56cb6a8418b24f55c2a642d0a904b00566ad52
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-09-17 15:16:47 +0530

    Create TestEnum1.java

---
+++ Datatypes/TestEnum1.java
@@ -0,0 +1,22 @@
+package Datatypes;
+
+public class TestEnum1 {
+
+	public static void main(String[] args) {
+		
+	    Month m = Month.January; 
+	    switch(m)
+		{
+		case January:
+			System.out.println(m);
+		break;
+		}
+
+	}
+
+}
+
+
+Result:-
+
+January
