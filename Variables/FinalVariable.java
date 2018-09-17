5f78a7f9ab16dfabf19dc952e09233a0b9e03236
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-09-17 19:13:16 +0530

    Create FinalVariable.java

---
+++ Variables/FinalVariable.java
@@ -0,0 +1,16 @@
+package Variables;
+
+public class FinalVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		final int number =100;
+		System.out.println(number);
+        // number=200;//Error cannot modify final
+        a=20;
+	}
+
+}
+
